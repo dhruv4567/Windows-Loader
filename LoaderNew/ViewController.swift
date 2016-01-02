213575c07fd793c5e6a8f69248c778b8e31bbb1f
dhruv khatri <khatridhruv4567@gmail.com>
2016-01-02 13:47:18 +0530

    Initial Commit

---
+++ LoaderNew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoaderNew
+//
+//  Created by Dhruv Khatri on 02/01/16.
+//  Copyright © 2016 Dhruv Khatri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
